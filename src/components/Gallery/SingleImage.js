26e746edd0d1a2c2669719241c3699841f4ffb54
Dennys <turtleluck20@gmail.com>
2021-03-20 20:56:59 +0200

    [Fix]: image link on landing page

--- src/components/Gallery/SingleImage.js
+++ src/components/Gallery/SingleImage.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
-import { db } from '../../firebase'
+import { db, auth } from '../../firebase'
 
 function SingleImage({ id, post }) {
-
   const [userProfile, setUserProfile] = useState(null)
 
   useEffect(() => {
@@ -23,22 +22,58 @@ function SingleImage({ id, post }) {
   }, [userProfile])
 
   return (
-    <div style={{height: '250px', backgroundColor: '#DBDBDB', boxShadow:'8px 10px 5px #88888f'}}>
-      <a style={{ color: '#8F8F8F' }} href={`/singleitem/${id}`}>
-        <img
-          key={id}
-          src={post.imageUrl}
-          style={{ width: '100%', height: 230, objectFit:'cover', borderColor:'#8f8f8f00', borderStyle: 'solid', borderWidth: '10px 10px 10px' }}
-          alt=''
-        />
-      </a>
+    <div
+      style={{
+        height: '250px',
+        backgroundColor: '#DBDBDB',
+        boxShadow: '8px 10px 5px #88888f'
+      }}>
+      {auth.currentUser ? (
+        <a style={{ color: '#8F8F8F' }} href={`/singleitem/${id}`}>
+          <img
+            key={id}
+            src={post.imageUrl}
+            style={{
+              width: '100%',
+              height: 230,
+              objectFit: 'cover',
+              borderColor: '#8f8f8f00',
+              borderStyle: 'solid',
+              borderWidth: '10px 10px 10px'
+            }}
+            alt=''
+          />
+        </a>
+      ) : (
+        <a style={{ color: '#8F8F8F' }} href='/signup'>
+          <img
+            key={id}
+            src={post.imageUrl}
+            style={{
+              width: '100%',
+              height: 230,
+              objectFit: 'cover',
+              borderColor: '#8f8f8f00',
+              borderStyle: 'solid',
+              borderWidth: '10px 10px 10px'
+            }}
+            alt=''
+          />
+        </a>
+      )}
       <div
         style={{
           display: 'flex',
           justifyContent: 'center',
           marginTop: '-10px'
         }}>
-        <p style={{ color: '#8F8F8F', whiteSpace: 'nowrap', fontFamily: "Segoe Script", fontWeight: 'bold' }}>
+        <p
+          style={{
+            color: '#8F8F8F',
+            whiteSpace: 'nowrap',
+            fontFamily: 'Segoe Script',
+            fontWeight: 'bold'
+          }}>
           {userProfile ? userProfile.firstName : ''}
         </p>
       </div>
