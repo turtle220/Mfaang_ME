7e1f46c7bd503793411148f255d1847a99d033d7
Dennys <turtleluck20@gmail.com>
2021-03-22 20:01:33 +0200

    [Fix]: hide firebase api keys using env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,14 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyBTgR7a4-Yzbc5v5kmp9qV3nAHNT4d5AGE',
-  authDomain: 'mfaang-2dfeb.firebaseapp.com',
-  databaseURL: 'https://mfaang-2dfeb.firebaseio.com',
-  projectId: 'mfaang-2dfeb',
-  storageBucket: 'mfaang-2dfeb.appspot.com',
-  messagingSenderId: '188452943792',
-  appId: '1:188452943792:web:7d175404f3f2bd36538875',
-  measurementId: 'G-WSVGYFL1EE',
-
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 
 const db = firebaseApp.firestore();
